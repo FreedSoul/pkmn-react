04dc9eb15d95683db69b7996d31b3a6cd8fcc481
FreedSoul <mattwork07@gmail.com>
2022-08-12 01:08:15 -0500

    moves with level working

--- components/PrintDetails.js
+++ components/PrintDetails.js
@@ -5,7 +5,7 @@ import useSWR from "swr"
 import { fetcher } from "./pokemon"
 
 function FetchDetails(name, url) {
-  const { data, error } = useSWR(name?`${url}${name}`:null, fetcher)
+  const { data, error } = useSWR(name ? `${url}${name}` : null, fetcher)
 
   return {
     details: data,
@@ -15,40 +15,74 @@ function FetchDetails(name, url) {
 }
 
 export default function PrintDetails({ name, url }) {
-  const abilities = FetchDetails(name, url)
-  console.log(typeof(abilities))
-  const weight = FetchDetails(name, url).weight
-  const height = FetchDetails(name, url).height
-  const stats = FetchDetails(name, url).stats
-  const moves = FetchDetails(name, url).moves
+  const details = FetchDetails(name, url).details
+
+  const abilities = details?.abilities.map((abilityItem, index) => {
+    return <li key={index}>{abilityItem.ability.name}</li>
+  })
+
+  const weight = details?.weight
+
+  const height = details?.height
+
+  const stats = details?.stats.map((statItem, index) => {
+    return (
+      <li key={index}>{statItem.stat.name + " - - " + statItem.base_stat}</li>
+    )
+  })
+
+    let movesLevel = details?.moves
+      .map((moveItem) => {
+        return [moveItem.move.name,moveItem.version_group_details
+          .map((item) => {
+            if (
+              item.version_group.name === "x-y" &&
+              item.move_learn_method.name === "level-up"
+            ) {
+              return item.level_learned_at
+            } else {
+              return 0
+            }
+          })
+          .filter((u) => u !== 0)
+      ]})
+      .filter((y) => JSON.stringify(y[1]) !== '[]').map(y => y.flat())
+      .map((q, index) => <li key={index}>{q[0] +'- - '+ ((q[1]&&q[2])??q[1])}</li>)
+
+    // console.log(movesLevel2, name, "level")
+
+    // console.log(movesLevel, name, "level")
+    // console.log(abilities)
+  // console.log(moves)
+  // console.log(moves)
+  console.log("-----------")
   return (
     <>
       {
-        <div className={styles.details}>
-          {/* <div className={styles.abilities}>
-            <h2>Abilities</h2>
-            <div className={styles.ability}>{abilities}</div>
-          </div> */}
+        // < className={styles.details}>
+        <div>
+          <div className={styles.abilities}>
+            <h3>Abilities</h3>
+            <div className={styles.ability}>{<ul>{abilities}</ul>}</div>
+          </div>
           <div className={styles.weight}>
-            <h2>weight</h2>
+            <h3>weight</h3>
             <div className={styles.weightvalue}>{weight}</div>
           </div>
           <div className={styles.height}>
-            <h2>height</h2>
+            <h3>height</h3>
             <div className={styles.heightvalue}>{height}</div>
           </div>
-          {/* <div className={styles.stats}>
+          <div className={styles.stats}>
             <h2>stats</h2>
             <div className={styles.statsvalue}>{stats}</div>
           </div>
-          <div className={styles.moves}>
-            <h2>moves</h2>
-            <div className={styles.movesvalue}>{moves}</div>
-          </div> */}
+          <div className={styles.movesLevel}>
+            <h2>movesLevel</h2>
+            <div className={styles.movesvalue}>{movesLevel}</div>
+          </div>
         </div>
       }
     </>
   )
 }
-
-
