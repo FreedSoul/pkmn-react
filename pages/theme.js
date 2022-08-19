d0aca73a1c2d1a834f264441fb0f3e37a88b2da9
FreedSoul <mattwork07@gmail.com>
2022-08-19 18:49:13 -0500

    fixing deploy to vercel, theme.js#2

--- pages/theme.js
+++ pages/theme.js
@@ -1,14 +1,30 @@
-// theme.js
-export default theme = {
-  colors: {
-    transparent: "transparent",
-    black: "#000",
-    white: "#fff",
-    gray: {
-      50: "#f7fafc",
-      // ...
-      900: "#171923",
-    },
-    // ...
+import { extendTheme } from "@chakra-ui/react"
+import { mode } from "@chakra-ui/theme-tools"
+
+const overrides = extendTheme({
+  styles: {
+    global: (props) => ({
+      body: {
+        fontFamily: "body",
+        color: mode("gray.800", "whiteAlpha.900")(props),
+        bg: mode("white", "gray.800")(props),
+        lineHeight: "base",
+      },
+    }),
   },
-}
+})
+
+// theme.js
+// export default theme = {
+//   colors: {
+//     transparent: "transparent",
+//     black: "#000",
+//     white: "#fff",
+//     gray: {
+//       50: "#f7fafc",
+//       // ...
+//       900: "#171923",
+//     },
+//     // ...
+//   },
+// }
