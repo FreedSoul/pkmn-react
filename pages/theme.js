b68dff4f3f57c726e185db824e1e1d192ec3da1f
FreedSoul <mattwork07@gmail.com>
2022-08-19 17:50:16 -0500

    fixing deploy to vercel, theme.js

--- pages/theme.js
+++ pages/theme.js
@@ -1,14 +1,14 @@
-// // theme.js
-// export default theme = {
-//   colors: {
-//     transparent: "transparent",
-//     black: "#000",
-//     white: "#fff",
-//     gray: {
-//       50: "#f7fafc",
-//       // ...
-//       900: "#171923",
-//     },
-//     // ...
-//   },
-// }
+// theme.js
+export default theme = {
+  colors: {
+    transparent: "transparent",
+    black: "#000",
+    white: "#fff",
+    gray: {
+      50: "#f7fafc",
+      // ...
+      900: "#171923",
+    },
+    // ...
+  },
+}
